10143be54dfcae706cf4afab9422b7b54149b542
Juan Ignacio <>
2020-12-07 00:23:21 -0600

    Termino el proyecto de JSparte 2 Ignacio CL

--- js/script.js
+++ js/script.js
@@ -1,59 +1,84 @@
 const wrapper = document.getElementById('wrapper');
+const modalul = document.getElementById('lista-padre');
+const modal = document.getElementById('background');
+const modalh2 = document.getElementById('Country-title');
+const modalimg = document.getElementById('bandera');
 function countries (Paises){
-  Paises.forEach(Pais => {
+  wrapper.innerHTML='';
+  for(let i = 0; i<Paises.length; i++){
     const title = document.createElement('a');
     title.setAttribute('class', 'astyle')
     title.setAttribute('href', '#')
-    title.innerHTML = `${Pais.name}`;
+    title.innerHTML = `${Paises[i].name}`;
     wrapper.appendChild(title);
-    title.addEventListener('click', (event)=>{
-      const actual = event.currentTarget;
-      const modal = document.getElementById('background');
-      const modalh2 = document.getElementById('Country-title');
-      const modalimg = document.getElementById('bandera');
-      const modalul = document.getElementById('lista-padre');
+    title.addEventListener('click', ()=>{
       modal.style.display = 'block';
       wrapper.style.display = 'none';
-      modalul.innerHTML = '';
-      modalh2.innerHTML = `${Pais.name}`;
-      modalimg.setAttribute('src', `${Pais.flag}`)
+      modalul.innerHTML='';
+      modalh2.innerHTML = `${Paises[i].name}`;
+      modalimg.setAttribute('src', `${Paises[i].flag}`)
+      modalimg.setAttribute('alt', 'Imagen del Pais')
       const lista1 = document.createElement('li');
-      lista1.innerHTML = `Capital = ${Pais.capital}`;
+      lista1.innerHTML = `Capital = ${Paises[i].capital}`;
       modalul.appendChild(lista1);
       const lista2 = document.createElement('li');
-      lista2.innerHTML = `Region = ${Pais.region}`;
+      lista2.innerHTML = `Region = ${Paises[i].region}`;
       modalul.appendChild(lista2);
       const lista3 = document.createElement('li');
-      lista3.innerHTML = `Subregion = ${Pais.subregion}`;
+      lista3.innerHTML = `Subregion = ${Paises[i].subregion}`;
       modalul.appendChild(lista3);
       const lista4 = document.createElement('li');
-      lista4.innerHTML = `Poblacion = ${Pais.population}`;
+      lista4.innerHTML = `Poblacion = ${Paises[i].population}`;
       modalul.appendChild(lista4);
       const lista5 = document.createElement('li');
-      lista5.innerHTML = `Aream2 = ${Pais.area}`;
+      lista5.innerHTML = `Aream2 = ${Paises[i].area}`;
       modalul.appendChild(lista5);
       const lista6 = document.createElement('li');
-      lista6.innerHTML = `Lenguaje = ${Pais.languages}`;
+      lista6.setAttribute('id', `Idioma${Paises[i].name}`);
       modalul.appendChild(lista6);
       const lista7 = document.createElement('li');
-      lista7.innerHTML = `Moneda = ${Pais.currencies}`;
+      lista7.setAttribute('id', `Coins${Paises[i].name}`);
       modalul.appendChild(lista7);
+      const idioma = document.getElementById(`Idioma${Paises[i].name}`);
+      const coins = document.getElementById(`Coins${Paises[i].name}`);
+      let idiom = '';
+      let money = '';
+      for(let n = 0; n<Paises[i].languages.length;n++){
+        const speak = `${Paises[i].languages[n].name} `;
+        idiom = idiom.concat(speak);
+      }
+      idioma.innerHTML= idiom;
+      for(let k = 0; k<Paises[i].currencies.length;k++){
+        const divisa = `${Paises[i].currencies[k].name} `;
+        money = money.concat(divisa);
+      }
+      coins.innerHTML= money;
       const out = document.getElementById('equis');
       out.addEventListener('click', () =>{
         modal.style.display = 'none';
         wrapper.style.display = 'block';
       })
     })
-  });
+  };
 }
-
-
-
-
-
-fetch('https://restcountries.eu/rest/v2/all')
+const form = document.getElementById('formulario');
+const input = document.getElementById('search');
+  fetch('https://restcountries.eu/rest/v2/all')
   .then((response) => response.json())
   .then((data) => {
-    console.log(data);
-    countries(data)
-  });
+    let listaPaises = data;
+    console.log(listaPaises);
+    countries(listaPaises);
+    form.addEventListener('input', (event)=>{
+    event.preventDefault();
+    let value = form.elements[0].value;
+    const PaisesEspecficos = listaPaises.filter((Pais)=>{
+      return Pais.name.includes(`${value}`)
+    })
+    if(PaisesEspecficos){
+      countries(PaisesEspecficos);
+    }
+  })
+});
+
+
